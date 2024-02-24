df9a44df8517f6489a11988616874e999ecd8d05
Denis Dinkov <denisdinkov7@gmail.com>
2024-02-24 16:11:29 +0200

    Transactions smart contract

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCounter;
+
+    event Transfer(address from, address reciever, uint amount, string message, uint timestamp, string keyword);
+
+    struct TransferStruct {
+        address sender;
+        address reciever;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transcations;
+
+     function addToBlockchain() public {
+
+    }
+
+    
+     function getAllTransactions() public view returns (TransferStruct[] memory) {
+        //return transactions
+    }
+
+    
+     function getTransactionCount() public view returns (uint256) {
+        //return transactionCount
+    }
+}
