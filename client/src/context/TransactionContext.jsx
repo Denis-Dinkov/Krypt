6f2c86490ae2bd110f9da627a3c1d3d77416d7cc
Denis Dinkov <denisdinkov7@gmail.com>
2024-02-26 09:06:52 +0200

    Connecting with metamask

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -23,8 +23,41 @@ const getEthereumContract = () => {
 };
 
 export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    if (!ethereum) return alert("Please install MetaMask 🦊");
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length) {
+      setCurrentAccount(accounts[0]);
+
+      //getAllTransaction()
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install MetaMask 🦊");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+
+      throw new Error("No ethereum object.");
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
-    <TransactionContext.Provider value={{ value: "test" }}>
+    <TransactionContext.Provider value={{ connectWallet }}>
       {children}
     </TransactionContext.Provider>
   );
